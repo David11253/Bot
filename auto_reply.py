5f07f494172d0736365fedca13876f9bf3fafe84
David11253 <minerusiapiter58@gmail.com>
2025-05-11 22:18:52 +0600

    Update auto_reply.py

--- auto_reply.py
+++ auto_reply.py
@@ -5,6 +5,9 @@ from telethon import TelegramClient, events
 api_id = os.getenv('29266838') # Получаем из переменной окружения и преобразуем в int
 api_hash = os.getenv('682fed56429bcc3db215c0ee7e34ec2d')   # Получаем из переменной окружения
 
+print("API ID:", api_id)  # Печать значений переменных для проверки
+print("API Hash:", api_hash)
+
 client = TelegramClient('auto_session', api_id, api_hash)
 
 @client.on(events.NewMessage(incoming=True))
