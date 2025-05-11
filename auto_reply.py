7663e6cbe96c6824d9ea95313b3cb1df37769fb9
David11253 <minerusiapiter58@gmail.com>
2025-05-11 22:27:45 +0600

    Update auto_reply.py

--- auto_reply.py
+++ auto_reply.py
@@ -1,16 +1,14 @@
 import os
 from telethon import TelegramClient, events
 
-# Получаем данные из переменных окружения
-api_id = int(os.getenv("29266838"))
-api_hash = os.getenv("682fed56429bcc3db215c0ee7e34ec2d")
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
 
-# Создание клиента Telethon
 client = TelegramClient('auto_session', api_id, api_hash)
 
 @client.on(events.NewMessage(incoming=True))
 async def handler(event):
-    if event.is_private:  # только личные сообщения
+    if event.is_private:
         await event.reply("Привет! Я сейчас занят, отвечу позже :)")
 
 client.start()
