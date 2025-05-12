8878866be966e82e3615dc1fb31770c3fece3a35
David11253 <minerusiapiter58@gmail.com>
2025-05-12 11:14:11 +0600

    Update auto_reply.py

--- auto_reply.py
+++ auto_reply.py
@@ -3,25 +3,26 @@ import os
 
 api_id = 29266838  # замените на ваш api_id
 api_hash = '682fed56429bcc3db215c0ee7e34ec2d'  # замените на ваш api_hash
+bot_token = '5910548947:AAE_7SOKfS_yU8CXPgsD8ClkWl4YSj-kGAs'  # замените на токен вашего бота
 
 # Удаление старой сессии, если она существует
 session_file = "auto_session.session"
 if os.path.exists(session_file):
     os.remove(session_file)
 
-client = TelegramClient(session_file, api_id, api_hash)
+# Используем токен бота для подключения
+client = TelegramClient(session_file, api_id, api_hash).start(bot_token=bot_token)
 
 @client.on(events.NewMessage)
 async def handler(event):
     # Проверка, является ли отправитель сообщением ботом
-    if event.sender_id != event.sender_id:  # Проверка, что это не бот
+    if not event.sender.bot:  # Проверка, что это не бот
         print(f"Получено сообщение от пользователя: {event.sender_id}")
 
         # Ответить на сообщение
         await event.reply("Привет, я бот и готов помочь!")
 
 async def main():
-    await client.start()
     print("Бот успешно авторизовался и теперь будет отвечать на сообщения, кроме ботов!")
 
 # Запуск бота и выполнение действия
